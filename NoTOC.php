9923f56281ae457701b0ce043b6756fa5e71955b
Pierre Rudloff <contact@rudloff.pro>
2016-09-24 19:06:07 +0000

    Applied fixes from StyleCI

--- NoTOC.php
+++ NoTOC.php
@@ -1,13 +1,14 @@
 <?php
 /**
- * NoTOC extension - Turns off the Table of Contents (TOC) by default on all pages
+ * NoTOC extension - Turns off the Table of Contents (TOC) by default on all pages.
+ *
  * @version 0.1.1 - 2013/12/11
  *
  * @link https://www.mediawiki.org/wiki/Extension:NoTOC Documentation
  * @link https://www.mediawiki.org/wiki/Extension_talk:NoTOC Support
  *
  * @ingroup Extensions
- * @package MediaWiki
+ *
  * @author Andrew Fitzgerald (Frantik)
  * @author Karsten Hoffmeyer (Kghbln)
  * @copyright (C) 2010 Andrew Fitzgerald
@@ -15,30 +16,32 @@
  */
 
 // check the call
-if ( !defined( 'MEDIAWIKI' ) ) {
-        die( 'This file is a MediaWiki extension and thus not a valid entry point.' );
+if (!defined('MEDIAWIKI')) {
+    die('This file is a MediaWiki extension and thus not a valid entry point.');
 }
 
 // provide information
-$wgExtensionCredits['parserhook'][] = array(
-	'path' => __FILE__,
-	'name' => 'NoTOC',
-        'author' => array(
-                'Andrew Fitzgerald', '...'
-                ),
-	'url' => 'https://www.mediawiki.org/wiki/Extension:NoTOC',
-	'descriptionmsg' => 'notoc-desc',
-	'version' => '0.1.1'
-);
+$wgExtensionCredits['parserhook'][] = [
+    'path'       => __FILE__,
+    'name'       => 'NoTOC',
+        'author' => [
+                'Andrew Fitzgerald', '...',
+                ],
+    'url'            => 'https://www.mediawiki.org/wiki/Extension:NoTOC',
+    'descriptionmsg' => 'notoc-desc',
+    'version'        => '0.1.1',
+];
 
 // show way to files
-$wgExtensionMessagesFiles['NoTOC'] = dirname( __FILE__ ) . '/NoTOC.i18n.php';
+$wgExtensionMessagesFiles['NoTOC'] = dirname(__FILE__).'/NoTOC.i18n.php';
 
 // register hooks
 $wgHooks['ParserClearState'][] = 'efMWNoTOC';
 
 // perform purpose
-function efMWNoTOC($parser) {
+function efMWNoTOC($parser)
+{
     $parser->mShowToc = false;
+
     return true;
 }
